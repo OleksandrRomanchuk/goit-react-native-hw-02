b612c0a3aa21de16dff3b330caccea7823139206
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-29 17:59:25 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
-import { TouchableWithoutFeedback, Keyboard } from "react-native";
-import AuthLayout from "./src/layouts/AuthLayout/AuthLayout";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
+import RoutesStack from "./src/RoutesStack";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./src/img/fonts/Roboto/Roboto-Bold.ttf"),
     "Roboto-Medium": require("./src/img/fonts/Roboto/Roboto-Medium.ttf"),
     "Roboto-Regular": require("./src/img/fonts/Roboto/Roboto-Regular.ttf"),
   });
@@ -15,24 +15,8 @@ export default function App() {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <AuthLayout />
-        <StatusBar style="auto" />
-      </View>
-    </TouchableWithoutFeedback>
+    <Provider store={store}>
+      <RoutesStack />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-
-    fontFamily: "Roboto",
-    fontSize: 16,
-    color: "#212121",
-  },
-});
