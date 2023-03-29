b73f998bd4f76ad0a0addf11b8ef89caab206b3a
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-29 17:53:21 +0300

    Create MapScreen.js

---
+++ src/screens/main/MapScreen/MapScreen.js
@@ -0,0 +1,7 @@
+import MainView from "../../../module/MainView/MainView";
+
+const MapScreen = ({ route, navigation }) => {
+  return <MainView route={route} navigation={navigation}></MainView>;
+};
+
+export default MapScreen;
