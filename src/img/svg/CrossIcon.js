79e19fa30ab0b13b49e3aae29d2ec620d8eef10b
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-29 17:56:22 +0300

    create icons

--- src/img/svg/CrossIcon.js
+++ src/img/svg/CrossIcon.js
@@ -1,26 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import Svg, { Path } from "react-native-svg";
 
-const CrossIcon = ({ isAvatarShown }) => {
+const CrossIcon = ({ fill }) => {
   return (
     <View
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: 25,
-        height: 25,
-        borderRadius: 13,
-        borderWidth: 1,
-        borderColor: isAvatarShown ? "#E8E8E8" : "#FF6C00",
-        transform: [{ rotate: isAvatarShown ? "45deg" : "0deg" }],
       }}
     >
       <Svg width={13} height={13} viewBox="0 0 13 13">
-        <Path
-          d="M7 0H6V6H0V7H6V13H7V7H13V6H7V0Z"
-          fill={isAvatarShown ? "#BDBDBD" : "#FF6C00"}
-        />
+        <Path d="M7 0H6V6H0V7H6V13H7V7H13V6H7V0Z" fill={fill} />
       </Svg>
     </View>
   );
