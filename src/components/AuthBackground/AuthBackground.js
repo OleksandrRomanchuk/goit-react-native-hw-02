20c248e7d55b921f58e12bd9c07473e528abdba6
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-29 17:59:19 +0300

    update AuthBackground

--- src/components/AuthBackground/AuthBackground.js
+++ src/components/AuthBackground/AuthBackground.js
@@ -1,14 +1,51 @@
-import { ImageBackground } from "react-native";
-import { bgImage } from "./AuthBackgroundStyles";
+import { useState, useEffect } from "react";
+
+import {
+  TouchableWithoutFeedback,
+  View,
+  ImageBackground,
+  Keyboard,
+} from "react-native";
+import { bgImage, container } from "./AuthBackgroundStyles";
 
 const AuthBackground = ({ children }) => {
+  const [isKeybordShown, setIsKeybordShown] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowSubscription = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setIsKeybordShown(true);
+      }
+    );
+    const keyboardDidHideSubscription = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setIsKeybordShown(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowSubscription.remove();
+      keyboardDidHideSubscription.remove();
+    };
+  }, []);
+
   return (
-    <ImageBackground
-      source={require("../../img/authBackground.jpg")}
-      style={bgImage}
+    <TouchableWithoutFeedback
+      style={[isKeybordShown && { marginBottom: -200 }]}
+      onPress={Keyboard.dismiss}
     >
-      {children}
-    </ImageBackground>
+      <>
+        <ImageBackground
+          source={require("../../img/authBackground.jpg")}
+          style={[bgImage, isKeybordShown && { marginBottom: -300 }]}
+        />
+        <View style={[container, isKeybordShown && { marginBottom: -200 }]}>
+          {children}
+        </View>
+      </>
+    </TouchableWithoutFeedback>
   );
 };
 
--- src/components/AuthBackground/AuthBackgroundStyles.js
+++ src/components/AuthBackground/AuthBackgroundStyles.js
@@ -2,9 +2,19 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   bgImage: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  container: {
+    position: "absolute",
+    bottom: 0,
     width: "100%",
-    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
   },
 });
 
-export const { bgImage } = styles;
+export const { bgImage, container } = styles;
