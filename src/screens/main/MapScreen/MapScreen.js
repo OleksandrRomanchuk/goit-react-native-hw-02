2a36d1c989bd3e8b5f3d76f01b61af85f1dc89dd
Oleksandr Romanchuk <novator0511@gmail.com>
2023-04-02 13:19:24 +0300

    Update MapScreen.js

--- src/screens/main/MapScreen/MapScreen.js
+++ src/screens/main/MapScreen/MapScreen.js
@@ -1,7 +1,36 @@
 import MainView from "../../../module/MainView/MainView";
+import MapView, { Marker } from "react-native-maps";
+import { useSelector } from "react-redux";
+import {
+  selectPosts,
+  selectCurrentPostId,
+} from "../../../redux/posts/postsSelectors";
 
 const MapScreen = ({ route, navigation }) => {
-  return <MainView route={route} navigation={navigation}></MainView>;
+  const currentPostId = useSelector(selectCurrentPostId);
+  const { posts } = useSelector(selectPosts);
+  const {
+    image: { coordinates },
+  } = posts.find((post) => post.id === currentPostId);
+
+  return (
+    <MainView route={route} navigation={navigation}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          ...coordinates,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        <Marker
+          coordinate={{
+            ...coordinates,
+          }}
+        />
+      </MapView>
+    </MainView>
+  );
 };
 
 export default MapScreen;
