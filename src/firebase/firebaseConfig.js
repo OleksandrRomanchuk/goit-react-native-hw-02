2aecb01740e6336fcda90c0520aef610b78aa82c
Oleksandr Romanchuk <novator0511@gmail.com>
2023-04-03 15:34:51 +0300

    Create firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6BMx08GK_ThVVgqgG4xSyHA7BNPwW4_k",
+  authDomain: "edu-react-native-app.firebaseapp.com",
+  projectId: "edu-react-native-app",
+  storageBucket: "edu-react-native-app.appspot.com",
+  messagingSenderId: "823840840749",
+  appId: "1:823840840749:web:ad68afad951c6b81302ca4",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
