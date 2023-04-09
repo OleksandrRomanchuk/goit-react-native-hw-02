a6519594706112e5e1b5a40baf9df24e672c504a
Oleksandr Romanchuk <novator0511@gmail.com>
2023-04-09 09:43:10 +0300

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -6,20 +6,24 @@ const authSlice = createSlice({
   initialState: {
     user: { name: "", email: "" },
     uid: "",
+    stateChange: false,
   },
   extraReducers: (builder) => {
     builder
       .addCase(register.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.uid = payload.uid;
+        state.stateChange = true;
       })
       .addCase(logIn.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.uid = payload.uid;
+        state.stateChange = true;
       })
       .addCase(logOut.fulfilled, (state) => {
         state.user = { name: "", email: "" };
         state.uid = "";
+        state.stateChange = false;
       });
   },
 });
