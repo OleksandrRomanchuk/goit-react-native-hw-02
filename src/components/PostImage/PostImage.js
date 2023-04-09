9920f14f3926a38799f674da4e96c785322e744e
Oleksandr Romanchuk <novator0511@gmail.com>
2023-04-09 09:41:49 +0300

    Update PostImage.js

--- src/components/PostImage/PostImage.js
+++ src/components/PostImage/PostImage.js
@@ -2,7 +2,7 @@ import { Image } from "react-native";
 import { image } from "./PostImageStyles";
 
 const PostImage = ({ source }) => {
-  return <Image source={{ uri: source.uri }} style={image} />;
+  return <Image source={{ uri: source }} style={image} />;
 };
 
 export default PostImage;
